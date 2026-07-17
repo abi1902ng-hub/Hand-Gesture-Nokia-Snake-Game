737c5b0a4b56fda72146e197a87b3a2ecb053c7c
Abinaya N G <abi1902ng@gmail.com>
2026-07-17 18:45:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Hand-Gesture-Nokia-Snake-Game/',
+  base: '/',
 
   plugins: [react()],
 
